3675a9afc5112d0d7afe370b87e50b32638c6999
Gaurav Kumar Patel <39117444+gauravpatelcse@users.noreply.github.com>
2018-10-29 11:19:03 +0530

    update image alt as per reviewer's comments

--- js/main.js
+++ js/main.js
@@ -170,10 +170,12 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `${restaurant.name} Restaurant`;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
+  name.setAttribute('tabindex', '0');
   li.append(name);
 
   const neighborhood = document.createElement('p');
@@ -187,6 +189,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute('aria-label',`Details about ${restaurant.name}`);
   li.append(more)
 
   return li
